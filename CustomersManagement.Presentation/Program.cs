54fcf6b9bd09c9956b6ec9f5155978fbd15ab7fb
MohamadAlturky <emailaddr>
2024-01-24 02:14:55 +0300

    Edit Code

--- CustomersManagement.Presentation/Program.cs
+++ CustomersManagement.Presentation/Program.cs
@@ -5,49 +5,56 @@ using CustomersManagement.Infrastructure.DBContext;
 using CustomersManagement.Infrastructure.RepositoriesImplemetation;
 using CustomersManagement.Presentation.ApiModels;
 
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>();
-builder.Services.AddScoped<IEntityRepository<Customer>, EntityRepository<Customer>>();
-builder.Services.AddScoped(typeof(CustomerService));
-
-var app = builder.Build();
-
-
-
-if (app.Environment.IsDevelopment())
+namespace CustomersManagement.Presentation;
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-
-app.MapGet("/customers", (CustomerService customerService) =>
-{
-    return customerService.GetCustomers();
-})
-.WithName("GetCustomers")
-.WithOpenApi();
-
-
-app.MapPost("/createCustomer", (CreateCustomerRequest createCustomerRequest ,CustomerService customerService) =>
-{
-   return customerService.insertCustomer(new Customer()
+    private static void Main(string[] args)
     {
-        CustomerId = createCustomerRequest.CustomerId,
-        FirstName = createCustomerRequest.FirstName,
-        LastName= createCustomerRequest.LastName,
-        Id = 0,
-        IsDeleted = false
-    });
-})
-.WithName("CreateCustomer")
-.WithOpenApi();
+        var builder = WebApplication.CreateBuilder(args);
 
 
-app.Run();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>();
+        builder.Services.AddScoped<IEntityRepository<Customer>, EntityRepository<Customer>>();
+        builder.Services.AddScoped(typeof(CustomerService));
+
+        var app = builder.Build();
+
+
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+
+        app.MapGet("/customers", (CustomerService customerService) =>
+        {
+            return customerService.GetCustomers();
+        })
+        .WithName("GetCustomers")
+        .WithOpenApi();
+
+
+        app.MapPost("/createCustomer", (CreateCustomerRequest createCustomerRequest, CustomerService customerService) =>
+        {
+            return customerService.insertCustomer(new Customer()
+            {
+                CustomerId = createCustomerRequest.CustomerId,
+                FirstName = createCustomerRequest.FirstName,
+                LastName = createCustomerRequest.LastName,
+                Id = 0,
+                IsDeleted = false
+            });
+        })
+        .WithName("CreateCustomer")
+        .WithOpenApi();
+
+
+        app.Run();
+    }
+}
